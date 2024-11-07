523198189b57812cedefe34a037638898fef54bf
Giuseppe Fasano <92933444+GiuseppeFasano@users.noreply.github.com>
2024-11-07 14:32:03 +0100

    Add files via upload

---
+++ client/lib/commonFunctions/keys.dart
@@ -0,0 +1,9 @@
+// This is the server address of airtown app:
+// 
+const String serverAddress = ""; // "server:port"
+
+// Load simulated user preferences:
+// - 1: user 1 simulated data
+// - 2: user 2 simulated data
+// - other: no simulated data
+const int simulationSetting = 0;
